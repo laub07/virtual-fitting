ef30f8473e45f5c8278ba8852951469b293270e6
박지소 <laub123@naver.com>
2026-03-24 14:06:34 +0900

    add spring boot project

---
+++ virtual-fitting/src/test/java/virtual_fitting/virtual_fitting/VirtualFittingApplicationTests.java
@@ -0,0 +1,13 @@
+package virtual_fitting.virtual_fitting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualFittingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
